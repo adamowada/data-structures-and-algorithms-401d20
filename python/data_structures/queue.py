8a212ab176bf4770784f984cb0494f0986506189
Adam Owada <adamowada@gmail.com>
2023-01-06 09:31:07 -0800

    stacks and queues

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,27 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.front = None
+        self.rear = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        # check to see if queue is empty!
+        if self.rear:
+            self.rear.next = Node(value)
+            self.rear = self.rear.next
+            return
+        self.rear = self.front = Node(value)
+
+    def dequeue(self):
+        dequeued = self.front
+        self.front = self.front.next
+        return dequeued.value
